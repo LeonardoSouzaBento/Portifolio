cfa4432b658b97a5abc7e262237488cd88cf4183
LeonardoSouzaBento <bentosouzaleonardo@gmail.com>
2026-03-09 13:49:17 -0300

    feat (disponibilizado o componente GitHubFilesExplorer para testes e uso futuro)

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,5 +1,5 @@
 interface Project {
-  id: 'supermercado' | 'catalogo' | 'css' | 'filmes' | 'pet-shop';
+  id: "supermercado" | "catalogo" | "css" | "filmes" | "pet-shop";
   name: string;
   description: string;
   image: string;
@@ -13,73 +13,73 @@ interface Project {
 
 export const projectsData: Project[] = [
   {
-    id: 'supermercado',
-    name: 'supermercado do bom',
+    id: "supermercado",
+    name: "supermercado do bom",
     description:
-      'Simula o processo de compra de um app de supermercado. Tem uma API integrada para login com Google e para captura de endereço por coordenadas via Google Maps...',
-    image: './projects/supermercado.png',
-    link: 'https://supermercadodobom.netlify.app/',
-    githubLink: 'https://github.com/LeonardoSouzaBento/SupermercadoDoBom',
+      "Simula o processo de compra de um app de supermercado. Tem uma API integrada para login com Google e para captura de endereço por coordenadas via Google Maps...",
+    image: "./projects/supermercado.png",
+    link: "https://supermercadodobom.netlify.app/",
+    githubLink: "https://github.com/LeonardoSouzaBento/SupermercadoDoBom",
     functions: {
-      '': '',
+      "": "",
     },
-    mainLabel: 'Styled Components',
-    labels: ['Firebase', 'Node.js', 'Express'],
-    resources: [''],
+    mainLabel: "Styled Components",
+    labels: ["Firebase", "Node.js", "Express"],
+    resources: [""],
   },
   {
-    id: 'catalogo',
-    name: 'catálogo de roupas',
+    id: "catalogo",
+    name: "catálogo de roupas",
     description:
-      'Um projeto feito em Next.js que permitiria a criação e hospedagem de catálogos de roupas para vários lojistas, todos sob o mesmo domínio...',
-    image: './projects/catalogo.png',
-    link: 'https://catalogo-de-roupas.vercel.app/',
-    githubLink: 'https://github.com/LeonardoSouzaBento/CatalogoDeRoupas',
+      "Um projeto feito em Next.js que permitiria a criação e hospedagem de catálogos de roupas para vários lojistas, todos sob o mesmo domínio...",
+    image: "./projects/catalogo.png",
+    link: "https://catalogo-de-roupas.vercel.app/",
+    githubLink: "https://github.com/LeonardoSouzaBento/CatalogoDeRoupas",
     functions: {
-      '': '',
+      "": "",
     },
-    mainLabel: 'Next.js',
-    labels: ['TypeScript', 'Tailwind CSS', 'Shadcn UI'],
+    mainLabel: "Next.js",
+    labels: ["TypeScript", "Tailwind CSS", "Shadcn UI"],
   },
   {
-    id: 'css',
-    name: 'gerador de CSS',
+    id: "css",
+    name: "gerador de CSS",
     description:
-      'Um site para gerar dinamicamente estilos em CSS para hierarquia tipográfica, para botões e para montar paletas de cores. Inspira a normalização e a estruturação de estilos...',
-    image: './projects/gerador-de-css.png',
-    link: 'https://geradordecss.netlify.app/',
-    githubLink: 'https://github.com/LeonardoSouzaBento/GeradorDeCSS',
+      "Um site para gerar dinamicamente estilos em CSS para hierarquia tipográfica, para botões e para montar paletas de cores. Inspira a normalização e a estruturação de estilos...",
+    image: "./projects/gerador-de-css.png",
+    link: "https://geradordecss.netlify.app/",
+    githubLink: "https://github.com/LeonardoSouzaBento/GeradorDeCSS",
     functions: {
-      '': '',
+      "": "",
     },
-    mainLabel: 'Tailwind CSS',
-    labels: ['Radix UI'],
+    mainLabel: "Tailwind CSS",
+    labels: ["Radix UI"],
   },
   {
-    id: 'filmes',
-    name: 'meus filmes favoritos',
+    id: "pet-shop",
+    name: "Leo Pet Shop",
     description:
-      'Confira uma lista de filmes calmos para pessoas empáticas e reflexivas, que valorizam a história, a justiça e a fé em ideais.',
-    image: './projects/filmes-favoritos.png',
-    link: 'https://filmesfavoritos.netlify.app/',
-    githubLink: 'https://github.com/LeonardoSouzaBento/MeusFilmesFavoritos',
+      "A landing page de visual belíssimo do grupo Leo Pet Shop. Feita para gerar conversões para o WhatsApp da empresa.",
+    image: "./projects/leopetshop.png",
+    link: "https://leopetshop.netlify.app",
+    githubLink: "https://github.com/LeonardoSouzaBento/Pet-ShopLandingPage",
     functions: {
-      '': '',
+      "": "",
     },
-    mainLabel: 'HTML e CSS',
-    labels: ['HTML', 'CSS', 'JavaScript'],
+    labels: ["Tailwind CSS", "Radix UI"],
   },
   {
-    id: 'pet-shop',
-    name: 'Leo Pet Shop',
+    id: "filmes",
+    name: "meus filmes favoritos",
     description:
-      'A landing page de visual belíssimo do grupo Leo Pet Shop. Feita para gerar conversões para o WhatsApp da empresa.',
-    image: './projects/leopetshop.png',
-    link: 'https://leopetshop.netlify.app',
-    githubLink: 'https://github.com/LeonardoSouzaBento/Pet-ShopLandingPage',
+      "Confira uma lista de filmes calmos para pessoas empáticas e reflexivas, que valorizam a história, a justiça e a fé em ideais.",
+    image: "./projects/filmes-favoritos.png",
+    link: "https://filmesfavoritos.netlify.app/",
+    githubLink: "https://github.com/LeonardoSouzaBento/MeusFilmesFavoritos",
     functions: {
-      '': '',
+      "": "",
     },
-    labels: ['Tailwind CSS', 'Radix UI'],
+    mainLabel: "HTML e CSS",
+    labels: ["HTML", "CSS", "JavaScript"],
   },
 ];
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,8 +5,10 @@ import {
   Projects,
   Technologies,
   TopNavBar,
-  PatternBackground
-} from '@/components/index';
+  PatternBackground,
+} from "@/components/index";
+// import { Index as GitHubFilesExplorer } from "github-project-file-explorer";
+// import "github-project-file-explorer/dist/style.css";
 
 interface HomeProps {
   resizeCount: number;
@@ -18,11 +20,22 @@ const Home = ({ resizeCount }: HomeProps) => {
       <TopNavBar />
       <main className={`px-3 md:px-6 lg:px-12 pb-8`}>
         <PatternBackground resizeCount={resizeCount} />
-        <Profile/>
+        <Profile />
         <Technologies />
         <AboutMe resizeCount={resizeCount} />
         <Projects resizeCount={resizeCount} />
         <Experience />
+        {/* <GitHubFilesExplorer
+          repoUrls={ [
+            "https://github.com/LeonardoSouzaBento/SupermercadoDoBom",
+            "https://github.com/LeonardoSouzaBento/CatalogoDeRoupas",
+            "https://github.com/LeonardoSouzaBento/GeradorDeCSS ",
+            "https://github.com/LeonardoSouzaBento/Pet-ShopLandingPage",
+            "https://github.com/LeonardoSouzaBento/MeusFilmesFavoritos",
+            "https://github.com/LeonardoSouzaBento/Portifolio",
+          ]}
+          cssWrapper="min-h-[600px]"
+        /> */}
       </main>
     </>
   );
